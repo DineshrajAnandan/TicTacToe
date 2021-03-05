fc9f7edbe6c10edc74a94f5131fd287e101d3235
Dineshraj <dineshrajanandan@gmail.com>
2021-03-05 16:37:02 +0530

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,126 @@
+function botTurn() {
+    if (botCheckRowWinningMove()) return;
+    if (botCheckColWinningMove()) return;
+    if (botCheckDiagWinningMove()) return;
+    if (botCheckCenter()) return;
+    if (botCheckOppCornersSameMove()) return;
+    if (botCheckRowColSingleOpponentFill()) return;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (!values[i][j]) {
+                fillPosition(i, j);
+                return;
+            }
+        }
+    }
+}
+
+function botCheckRowWinningMove() {
+    for (let i = 0; i < 3; i++) {
+        let rowArr = values[i];
+        let xCnt = rowArr.filter(x => x == 'X').length;
+        let oCnt = rowArr.filter(o => o == 'O').length;
+        if ((xCnt == 2 && oCnt == 0) || (oCnt == 2 && xCnt == 0)) {
+            fillPosition(i, rowArr.indexOf(''));
+            return true;
+        }
+    }
+    return false;
+}
+
+function botCheckColWinningMove() {
+    for (let i = 0; i < 3; i++) {
+        let colArr = [values[0][i], values[1][i], values[2][i]];
+        let xCnt = colArr.filter(x => x == 'X').length;
+        let oCnt = colArr.filter(o => o == 'O').length;
+        if ((xCnt == 2 && oCnt == 0) || (oCnt == 2 && xCnt == 0)) {
+            fillPosition(colArr.indexOf(''), i);
+            return true;
+        }
+    }
+    return false;
+}
+
+function botCheckCenter() {
+    if (!values[1][1]) {
+        fillPosition(1, 1);
+        return true;
+    }
+    return false;
+}
+
+function botCheckDiagWinningMove() {
+    let leftDiagArr = [values[0][0], values[1][1], values[2][2]];
+    let xCnt = leftDiagArr.filter(x => x == 'X').length;
+    let oCnt = leftDiagArr.filter(o => o == 'O').length;
+    if ((xCnt == 2 && oCnt == 0) || (oCnt == 2 && xCnt == 0)) {
+        let ind = leftDiagArr.indexOf('');
+        fillPosition(ind, ind);
+        return true;
+    }
+    let rightDiagArr = [values[0][2], values[1][1], values[2][0]];
+    xCnt = rightDiagArr.filter(x => x == 'X').length;
+    oCnt = rightDiagArr.filter(o => o == 'O').length;
+    if ((xCnt == 2 && oCnt == 0) || (oCnt == 2 && xCnt == 0)) {
+        let ind = rightDiagArr.indexOf('');
+        let ind2 = (ind == 1) ? 1 : (ind == 2) ? 0 : 2;
+        fillPosition(ind, ind2);
+        return true;
+    }
+    return false;
+}
+
+function botCheckOppCornersSameMove() {
+    let botsOpponent = botIsX ? 'O' : 'X';
+    if (values[0][0] == botsOpponent && values[2][2] == botsOpponent && botFillInAnyPlusPlaces()) {
+        return true;
+    }
+    if (values[0][2] == botsOpponent && values[2][0] == botsOpponent && botFillInAnyPlusPlaces()) {
+        return true;
+    }
+    return false;
+
+}
+
+function botFillInAnyPlusPlaces() {
+    let plusBoxesVal = [values[0][1], values[1][2], values[2][1], values[1][0]];
+    let ind = plusBoxesVal.indexOf('');
+    switch (ind) {
+        case 0:
+            fillPosition(0, 1);
+            return true;
+        case 1:
+            fillPosition(1, 2);
+            return true;
+        case 2:
+            fillPosition(2, 1);
+            return true;
+        case 3:
+            fillPosition(1, 0);
+            return true;
+        default:
+            return false;
+    }
+}
+
+function botCheckRowColSingleOpponentFill() {
+    let botsOpponent = botIsX ? 'O' : 'X';
+    for (let i = 0; i < 3; i++) {
+        let oppCnt = values[i].filter(x => x == botsOpponent).length;
+        let emptyCnt = values[i].filter(x => !x).length;
+        if (oppCnt == 1 && emptyCnt == 2) {
+            fillPosition(i, values[i].indexOf(''));
+            return true;
+        }
+    }
+    for (let i = 0; i < 3; i++) {
+        let colArr = [values[0][i], values[1][i], values[2][i]];
+        let oppCnt = colArr.filter(x => x == botsOpponent).length;
+        let emptyCnt = colArr.filter(x => !x).length;
+        if (oppCnt == 1 && emptyCnt == 2) {
+            fillPosition(colArr.indexOf(''), i);
+            return true;
+        }
+    }
+    return false;
+}
---
+++ main.js
@@ -0,0 +1,184 @@
+let values = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+];
+let xoToggle = true; // lets keep true for 'X' and false for 'O'
+let totalNoOfMoves = 0;
+let matchComplete = false;
+let playWithBot = false;
+let botIsX = false;
+let botsValue = 'X';
+let botIsPlaying = false;
+
+document.onreadystatechange = () => {
+    if (document.readyState == 'complete') {
+        initializeClickEvents();
+        playerCheck();
+    }
+};
+
+function initializeClickEvents() {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            document.getElementById(`${i}${j}`).addEventListener('click', () => {
+                if (botIsPlaying || matchComplete) return;
+                fillPosition(i, j);
+            });
+        }
+    }
+}
+
+function changeBotsValue(value) {
+    if (!playWithBot) return;
+    botsValue = value;
+    botIsX = botsValue == 'X';
+    if (botIsX) {
+        botIsPlaying = true;
+        botTurn();
+        botIsPlaying = false;
+    }
+}
+
+function fillPosition(i, j) {
+    let presentText = document.getElementById(`${i}${j}`).innerText.trim();
+    let fillValue = xoToggle ? 'X' : 'O'
+    if (!presentText) {
+        totalNoOfMoves++;
+        document.getElementById(`${i}${j}`).innerText = values[i][j] = fillValue;
+        checkWinningMove(fillValue);
+        xoToggle = !xoToggle;
+        playerCheck();
+    }
+}
+
+function playerCheck() {
+    let currPlayerIdName = xoToggle ? 'x-stat' : 'o-stat';
+    let oppPlayerIdName = xoToggle ? 'o-stat' : 'x-stat';
+
+    document.getElementById(currPlayerIdName).style.background = 'aqua';
+    document.getElementById(oppPlayerIdName).style.background = 'white';
+
+    checkBotsTurn();
+}
+
+function checkBotsTurn() {
+    if (playWithBot && botIsX == xoToggle) {
+        botIsPlaying = true;
+        setTimeout(() => {
+            botTurn();
+            botIsPlaying = false;
+        }, 500);
+    }
+}
+
+function checkWinningMove(fillValue) {
+    if (winningCheck(fillValue)) {
+        document.getElementById('winner-display-div').style.display = 'block';
+        document.getElementById('winner-display').innerText = `${fillValue} won`;
+        matchComplete = true;
+    }
+    else if (totalNoOfMoves >= 9) {
+        document.getElementById('winner-display-div').style.display = 'block';
+        document.getElementById('winner-display').innerText = `Draw`;
+        matchComplete = true;
+    }
+
+}
+
+function winningCheck(fillValue) {
+    let toCheck = ['row', 'col', 'diag'];
+    for (let t = 0; t < toCheck.length; t++) {
+        if ((toCheck[t] == 'row' || toCheck[t] == 'col') &&
+            rowOrColWinningCheck(fillValue, toCheck[t])) {
+            return true;
+        }
+        else if (toCheck[t] == 'diag' &&
+            (leftDiagWinningCheck(fillValue) || rightDiagWinningCheck(fillValue))) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function leftDiagWinningCheck(fillValue) {
+    for (let i = 0; i < 3; i++) {
+        if (values[i][i] != fillValue) return false;
+    }
+    fillWinningCells('leftDiag');
+    return true;
+}
+
+function rightDiagWinningCheck(fillValue) {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (i + j == 2 && values[i][j] != fillValue) return false;
+        }
+    }
+    fillWinningCells('rightDiag');
+    return true;
+}
+
+function rowOrColWinningCheck(fillValue, toCheck) {
+    for (let i = 0; i < 3; i++) {
+        let matchFound = true;
+        for (let j = 0; j < 3; j++) {
+            matchFound = toCheck == 'row' ?
+                values[i][j] == fillValue : values[j][i] == fillValue;
+            if (!matchFound) break;
+        }
+        if (matchFound) {
+            fillWinningCells(toCheck, i);
+            return true;
+        }
+    }
+    return false;
+}
+
+function fillWinningCells(toFill, index = -1) {
+    if (toFill == 'row' || toFill == 'col') {
+        for (let i = 0; i < 3; i++) {
+            let id = toFill == 'row' ? `${index}${i}` : `${i}${index}`;
+            document.getElementById(id).style.background = 'grey';
+        }
+        return;
+    }
+    if (toFill == 'leftDiag') {
+        for (let i = 0; i < 3; i++) {
+            document.getElementById(`${i}${i}`).style.background = 'grey';
+        }
+        return;
+    }
+    if (toFill == 'rightDiag') {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (i + j == 2) document.getElementById(`${i}${j}`).style.background = 'grey';
+            }
+        }
+        return;
+    }
+}
+
+function restartGame() {
+    matchComplete = false;
+    xoToggle = true;
+    totalNoOfMoves = 0;
+    botIsX = false;
+    botsValue = 'X';
+    values = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    ];
+    document.querySelectorAll('td').forEach(elem => {
+        elem.style.background = 'white';
+        elem.innerText = '';
+    });
+    document.getElementById('winner-display-div').style.display = 'none';
+    playerCheck();
+}
+
+function togglePlayWithBot(checkElem) {
+    restartGame();
+    playWithBot = checkElem.checked;
+}
